8c3a2c976e3b32fdc96e39ec8e2ee02ba5e05cc1
kkirti17 <106263358+kkirti17@users.noreply.github.com>
2022-05-25 21:20:15 +0530

    Delete func.py

--- func.py
+++
@@ -1,9 +0,0 @@
-# define a function that accepts roll number and returns whether the student is present or not
-def detail(roll):
-    x=[1,23,33,43,44]
-    if roll in x:
-        print(f"roll number {roll} is present")
-    else:
-        print(f"roll number {roll} is absent")
-roll = int(input("enter roll number"))
-detail(roll)
