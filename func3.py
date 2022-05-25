09198f5fc076eb9e19f56108fc79cb551d868a69
kkirti17 <106263358+kkirti17@users.noreply.github.com>
2022-05-25 21:27:15 +0530

    Delete func3.py

--- func3.py
+++
@@ -1,6 +0,0 @@
-# wap in python to find the si on agiven principle & time, where the rate is defined by the user.
-def si(rate):
-    si=(1000*5*rate)/100
-    print(si)
-rate=float(input("enter rate"))
-si(rate)
