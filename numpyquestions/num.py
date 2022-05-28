9186855d7f9394139454088e944904c532110380
kkirti17 <106263358+kkirti17@users.noreply.github.com>
2022-05-28 12:27:52 +0530

    Create num.py

---
+++ numpyquestions/num.py
@@ -0,0 +1,5 @@
+import numpy as np
+a = np.arange(6).reshape((2,3))
+print("origianl flattened array: ",a)
+print("maximum value of the flattened array: ",np.max(a))
+print("minimum value of the flattened array:",np.min(a))
