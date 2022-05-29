6e20ab25abf44b754e05c1a1b17e55fd14049753
kkirti17 <106263358+kkirti17@users.noreply.github.com>
2022-05-29 19:23:39 +0530

    python program to find the values of trignometric functions

---
+++ modules , file handling ,exception handling/math4.py
@@ -0,0 +1,6 @@
+#python program to find the values of trignometric functions
+import math
+n=int(input("enter a number"))
+print("sin(",n,")=",math.sin(n))
+print("cos(",n,")=",math.cos(n))
+print("tan(",n,")=",math.tan(n))
