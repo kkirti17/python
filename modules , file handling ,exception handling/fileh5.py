407bec545eb88c4a150040bb4eb037e8bf554059
kkirti17 <106263358+kkirti17@users.noreply.github.com>
2022-05-29 19:15:26 +0530

    python program to read last n lines of a file

---
+++ modules , file handling ,exception handling/fileh5.py
@@ -0,0 +1,15 @@
+#python program to read last n lines of a file
+file=open('kirtiiii.txt','w')
+file.write("my name is kirti bhardwaj hello")
+c=0
+with open("kirtiiii.txt",'r') as file:
+    line=file.readlines()
+    for i in line:
+        word=i.split()
+    print(word)
+    for j in word:
+        c=c+1
+    print("total no. of words are ",c)
+longword=max(word,key=len)
+print(longword)
+file.close()
